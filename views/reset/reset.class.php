786bd132a42ffe381a1f5ba7817abecbdcb98cf3
pvarney13 <pvarney@iu.edu>
2022-03-31 17:29:41 -0400

    edit register class

--- views/reset/reset.class.php
+++ views/reset/reset.class.php
@@ -8,6 +8,7 @@
  * logs in. It should also make the username field read-only. The form should use POST method
  * to submit data. If the user is currently not logged in, display an error message instead.
  */
+require 'Lab08/Untitled/models/user_model.class.php';
 
 class ResetView extends View
 {
@@ -15,15 +16,30 @@ class ResetView extends View
     {
         //add header from parent class
         parent::header();
+
+        if (verify_user() == true){
 ?>
 
         <!-- page specific content starts -->
 
         <div class="top-row">reset password</div>
 
+        <form action="index.php?action=do_reset" method="post">
+            <input style="width: 500px; margin-left: 25px" type="text" name="username" value="<?php $_COOKIE['username']?>" readonly><br>
+
+            <input style="width: 500px; margin-left: 25px" type="password" name="new_password" placeholder="Enter New Password"><br>
+
+            <input type="submit" style="width: 505px; margin-left: 25px; padding: 5px;background-color: #333333; color: white" value="RESET PASSWORD">
+        </form>
         <!-- page specific content ends -->
 
         <?php
+        }else {
+            echo 'Please Login First';
+            echo '<div> 
+                        <span>Already have an account? <a href="index.php?action=login">Login</a></span>
+                  </div>';
+        }
         //call the footer method defined in the parent class to add the footer
         parent::footer();
     }
