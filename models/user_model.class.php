538949d0d569e0fd0bbd715e189de7d632bee7a2
kavthakk <77465190+kavthakk@users.noreply.github.com>
2022-03-31 10:03:35 -0400

    foundations of kavin

---
+++ models/user_model.class.php
@@ -0,0 +1,47 @@
+<?php
+
+//This file defines a class named UserModel with four public methods.
+
+class UserModel {
+    //any private attributes
+    //the four public methods
+    public function add_user() {
+        //retrieve user details from registration form and add into users table in the usersystem database.
+
+        // The method returns true if the insertion is successful or false if it fails.
+        
+        // Passwords need to be hashed before they are stored into the database.
+        // To hash a password, call password_hash function. Please refer to http://php.net/manual/en/function.password-hash.php.
+
+    }
+
+    public function verify_user() {
+        //retrieve a user’s username and password from the login form and then verify them again a database record.
+
+        // If both username and password are valid,create a temporary cookie to store the username and return true;
+
+        // If either username or password is invalid, return false.
+
+        // To verify a hashed password, call password_verify function. Please refer to http://php.net/manual/en/function.password-verify.php.
+
+    }
+
+    public function logout() {
+        //logout a user by destroying the temporary cookie created when the user signs in.
+
+        //The method should return true.
+
+    }
+
+    public function reset_password() {
+        //retrieve a user’s username and password from the password reset form and
+
+        // then update the user’s password in the database.
+
+        // Make sure the password is hashed before it gets updated.
+
+        // Return true if the password is successfully updated or false otherwise.
+
+    }
+
+}
