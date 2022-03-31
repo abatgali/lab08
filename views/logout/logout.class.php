841c184aeac7754ee860fcda912d7b752a1cbddb
Sierra Braun <braundsig@gmail.com>
2022-03-31 11:42:02 -0400

    more editing

--- views/logout/logout.class.php
+++ views/logout/logout.class.php
@@ -1,17 +1,34 @@
 <?php
 
+/**
+ * Author: Sierra Braun
+ * Date: 3/29/22
+ * File: logout.class.php
+ * Description:  the view file for the logout
+ */
 
-class LogoutView extends View{
+class LogoutVerifyView extends View{
     public function display(){
-        //add header
+
+        //display header
         parent::header();
+        ?>
+
+        <!--        //page content starts-->
+
+        <div class="top-row">LOGIN</div>
+
+        <div class="middle-row">You have successfully logged out.</div>
+
+        <!--hyperlinks-->
+        <div class="bottom-row">
+            <span style="float: left">Already have an account? <a href="index.php?action=login">Login</a> </span>
+            <span style="float: right">Don't have an account? <a href="index.php?">Register</a> </span>
+        </div>
 
 
-
-
-
-
-        //add footer
+        <?php
+        //display footer
         parent::footer();
     }
 }
