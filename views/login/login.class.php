737d9a4ce99e3d9b8bb226b5386361218f57a943
Sierra Braun <braundsig@gmail.com>
2022-03-31 10:27:01 -0400

    edit login/out

---
+++ views/login/login.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Author: Sierra Braun
+ * Date: 3/29/22
+ * File: login.class.php
+ * Description:  the view file for the login
+ */
+
+class LoginView extends View{
+    public function display(){
+
+        //display header
+        parent::header();
+
+        echo '';
+
+
+
+        //display footer
+        parent::footer();
+    }
+}
---
+++ views/logout/logout.class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class LogoutView extends View{
+    public function display(){
+        //add header
+        parent::header();
+
+
+
+
+
+
+        //add footer
+        parent::footer();
+    }
+}
